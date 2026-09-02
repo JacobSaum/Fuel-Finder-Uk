d4c566f4f16b9a1f12e535b8e58973f115f63f75
Jacob Saum <j.saum@rgu.ac.uk>
2026-09-02 16:52:16 +0100

    Added Error handling

--- searchResults.py
+++ searchResults.py
@@ -2,8 +2,16 @@ from geocoding import geocodingLogic
 from sorting_scripts.priceSorting import sortFuelPrice, getStationData, FUEL_COLUMNS
 from sorting_scripts.distanceSorting import sortDistance
 from sorting_scripts.valueSorting import sortValue
+import re
+
+def isValidPostcode(postcode):
+    pattern = r"^[A-Z]{1,2}\d[A-Z\d]?\s?\d[A-Z]{2}$"
+    return bool(re.match(pattern, postcode.strip().upper()))
 
 def fuelSearch(fuelType, postcode, searchRadius, sortby="price"):
+
+    if isValidPostcode(postcode) == False:
+        raise ValueError(f"{postcode} is not a valid postcode.")
 
     valid_stations = geocodingLogic(postcode, searchRadius)
 
@@ -28,5 +36,8 @@ def fuelSearch(fuelType, postcode, searchRadius, sortby="price"):
 
     else: 
         raise ValueError(f"unknown sortby option: {sortby}")
+
+    if len(results) == 0:
+        raise LookupError(f"No Petrol Stations Within Search Radius.")
 
     return results
